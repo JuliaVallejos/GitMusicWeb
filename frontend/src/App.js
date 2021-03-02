61737cfea6ea0c5523be022ad96df00c6a5bc70f
lucia <pereyra.lucia@gmail.com>
2021-03-02 01:06:29 -0300

    creación sign in, instalo librerías rsuite, paquete redux y axios, google icons, router -falta sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './signIn.css';
+import signIn from './components/SignIn'
+import React from 'react'
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+        <Switch>
+        <Route path='/'component={signIn}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/SignIn.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react'
+// import { connect } from 'react-redux'
+import GoogleLogin from 'react-google-login'
+import { FaEye } from "react-icons/fa";
+import { Link } from 'react-router-dom'
+import { Alert, Message } from 'rsuite';
+
+
+const SignIn = (props) => {
+
+    // const { history, signIn, loggedUser } = props
+    const [user, setUser] = useState({})
+    const [errores, setErrores] = useState('')
+    const [hidden, setHidden] = useState(true)
+    // useEffect(() => {
+    //     if (loggedUser !== null)
+    //         setTimeout(() => {
+    //             history.push('/')
+    //         }, 3000)
+    // }, [loggedUser])
+
+    const readInput = e => {
+        const value = e.target.value
+        const property = e.target.name
+        setUser({
+            ...user,
+            [property]: value
+        })
+    }
+    const enterKeyboard = e => {
+        //El numero 13 seria la tecla enter, si fue presionada envio la validacion
+        //como si fuera el boton sign in
+        if (e.charCode === 13) {
+            Validate(e)
+        }
+    }
+    const Validate = async e => {
+        setErrores('')
+        if (!user.email || !user.password) {
+            setErrores('Todos los campos son requeridos')
+        } else {
+            console.log("hay errores")
+            // const response = await signIn(user)
+            // if (response && !response.success) {
+            //     setErrores(response.message)
+            }
+            // if (loggedUser !== null)
+            //     setTimeout(() => {
+            //         props.history.push('/')
+            //     }, 3000)
+        // }
+    }
+    // const responseGoogle = async response => {
+    //     if (response.error) {
+    //     } else {
+    //         const res = await props.signIn({
+    //             email: response.profileObj.email,
+    //             password: `Aa${response.profileObj.googleId}`,
+    //             google: 'true'
+    //         }
+    //         )
+    //         if (res && !res.success) {
+    //             Alert.error('Intente nuevamente')
+    //         }
+    //     }
+    // }
+    return (
+        <div className="registro">
+            <div className="formulario">
+                <h2>Acceder</h2>
+                <p>¡Hola! Iniciá sesión con tu cuenta</p>
+                {errores !== '' && <Message type='info' description={errores} style={{ marginBottom: '2vh' }} />}
+                <div className="inputDiv">
+                    <input onKeyPress={enterKeyboard} type="text" autoComplete="nope" name="email" placeholder="Ingrese su dirección de correo electrónico" onChange={readInput} />
+                </div>
+                <div className="inputDiv">
+                    <input onKeyPress={enterKeyboard} type={hidden ? "password" : " text"} name="password" placeholder="Ingrese su contraseña" onChange={readInput} />
+                    < FaEye className="eye" onClick={() => setHidden(!hidden)} />
+                </div>
+                <button className="enviar" onClick={Validate}>Acceder</button>
+                <Link to='/forgotpassword' className='navBarLinks' style={{ margin: '2vh' }}>
+                    <h6>¿Olvidaste tu contraseña?</h6>
+                </Link>
+                <GoogleLogin
+                    clientId="225799266122-1ec8ttmtjou3vupf7qk9kqdc98lvtjmj.apps.googleusercontent.com"
+                    buttonText="Iniciar sesión con Google"
+                    // onSuccess={responseGoogle}
+                    // onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+            </div>
+        </div>
+    )
+}
+// const mapStateToProps = state => {
+//     return {
+//         loggedUser: state.userR.loggedUser
+//     }
+// }
+// const mapDispatchToProps = {
+//     signIn: userActions.signIn
+// }
+export default SignIn
+
+// client secret google rPSbotRqanmaxXrIsITQ5cwK
