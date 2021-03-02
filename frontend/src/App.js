89862314f1fb5ad0925d7f042d42ad9d556b3ae0
Matias Juncos <matijuncos@gmail.com>
2021-03-01 22:20:52 -0300

    primera maquetación de componentes cards, nav, landing, cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import LandingPage from './components/LandingPage';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import CartIcon from './components/CartIcon';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<>
+<Router>
+<NavBar/>
+<CartIcon/>
+<Switch>
+  <Route exact path='/' component={LandingPage}/>
+  
+</Switch>
+</Router>
+<Footer/>
+</>
+  )
 }
 
 export default App;
