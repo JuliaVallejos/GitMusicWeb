76cf48882cd3060d997c5054234cf7e76835e691
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 10:38:40 -0300

    Backend a heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 const host = process.env.HOST || '0.0.0.0'
 
-app.listen(port, host,  console.log('app listening'))
+app.listen(port, host,  () => console.log(`App listening on port ${port}`))
