449a412d89cce97fd8882cb15f05708be252203c
BuildTools <unconfigured@null.spigotmc.org>
2021-03-07 21:10:37 -0300

    Limitaciones de productos según el stock agregadas.

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -4,35 +4,38 @@ import { FaShoppingCart } from 'react-icons/fa'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import shoppingCartActions from '../Redux/actions/shoppingCartActions'
+import {Alert} from 'rsuite'
 
-const Product = ({product,addProductShoppingCart}) =>{
-  console.log({product})
+const Product = ({product,addProductShoppingCart,shoppingCart}) =>{
  const [add,setAdd]= useState(false)
     
- const addProductCart = async() =>{
-  alert('Agregado al carrito')
-  const respuesta=await addProductShoppingCart({idProduct:product._id,quantity:1, product})
-  console.log(respuesta.response)
- }
+ const addProductCart = async(product) =>{
+    const filterProductCart = shoppingCart.filter(productF => productF.idProduct === product._id)
+    if(filterProductCart.length!==0 && (filterProductCart[0].product.stock<(filterProductCart[0].quantity+1))){
+      Alert.warning(`No podes exceder el stock(${filterProductCart[0].product.stock}) de este articulo.`,3000)
+    }else{
+      Alert.success('Agregado al carrito.',3500)
+      const respuesta=await addProductShoppingCart({idProduct:product._id,quantity:1, product})
+    }
+  }
   if(product) {
        return(
-        <div className='productContainer'>
-          <Link to={'/product/'+ product._id} className='productPic' style={{backgroundImage:`url(${product.arrayPic[0]})`}}>
-             {/* Product Image View */}
-          </Link>
-            {/* -- Price View */}
-            <div className="cardBottom">
-              <h4 className='productTitle'>{product.name}</h4>            
-            <div className='productPrice'>
-                <h6>{`$${product.price}`}</h6>
-                <div onMouseOver={()=>setAdd(true)} onMouseOut={()=>setAdd(false)} onClick={addProductCart} className='iconCart'>
-                  <FaShoppingCart/>
-                  {/* {add&&<div className='toolTip'><p className='add'>Agregar al carrito</p></div>} */}
-              </div>
-              <div>{add&&<p className='add'>Agregar al carrito</p>}</div>
-              
+      <div className='productContainer'>
+        <Link to={'/product/'+ product._id} className='productPic' style={{backgroundImage:`url(${product.arrayPic[0]})`}}>
+            {/* Product Image View */}
+        </Link>
+          {/* -- Price View */}
+        <div className="cardBottom">
+          <h4 className='productTitle'>{product.name}</h4>            
+          <div className='productPrice'>
+              <h6>{`$${product.price}`}</h6>
+              <div onMouseOver={()=>setAdd(true)} onMouseOut={()=>setAdd(false)} onClick={()=>addProductCart(product)} className='iconCart'>
+                <FaShoppingCart/>
+                {/* {add&&<div className='toolTip'><p className='add'>Agregar al carrito</p></div>} */}
             </div>
-            </div>
+            <div>{add&&<p className='add'>Agregar al carrito</p>}</div>
+          </div>
+        </div>
       </div>
       
     );     
@@ -42,7 +45,12 @@ const Product = ({product,addProductShoppingCart}) =>{
         )
     }
 }
+const mapStateToProps = state =>{
+  return{
+      shoppingCart:state.shoppingR.shoppingCart
+  }
+}
 const mapDispatchToProps={
   addProductShoppingCart:shoppingCartActions.addProductShoppingCart
 }
-export default connect(null,mapDispatchToProps)(Product)
+export default connect(mapStateToProps,mapDispatchToProps)(Product)
