6da41c0ea597974fb8f71f18d82aadbd6964559b
Julia Vallejos <jval.benes@gmail.com>
2021-03-07 23:23:57 -0300

    corregi mi mongo

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -7,14 +7,16 @@ const emailController = {
    sendEmailCart: async (req, res) =>{
 
        const {email,dataCart} =req.body
-       console.log(req.body.dataCart)
-       console.log(req.body.dataCart.userData)
-
-       const userAwait = await User.findOne({email:email})
+       console.log(dataCart)
+       console.log(email)
+    try{
+        const userAwait = await User.findOne({email:email})
         if(!userAwait){ 
+            console.log('no usuario')
             res.json({success: false, errors:[{message:"User don't exist"}]})
         }
-        else{     
+        else{    
+            console.log('hay usuario') 
         var transporter = nodemailer.createTransport({
             port: 465,
             host: 'smtp.gmail.com',
@@ -33,8 +35,7 @@ const emailController = {
             to: email,
             subject:"¡Bienvenido a GitMusic! Gracias por su compra",
             html: `<div style="text-align:center; padding:20px; min-heigth: 250px; background-color:#11050F">
-                        <h1 style="color:#FFB5FF">Estos son los datos de su compra</h1>
-                       
+                        <h1 style="color:#FFB5FF">Estos son los datos de su compra</h1>  
                     </div>`
         }
         transporter.sendMail(mailOptions, (error, info) =>{
@@ -45,7 +46,12 @@ const emailController = {
                 res.status(200).json({respuesta:req.body})
             }
         })} 
-     */
+    
+      
+    }catch(error){
+        console.log('error')
+        console.log(error)
+        res.json({success:false,error})
     }
-}
+}}
 module.exports = emailController
