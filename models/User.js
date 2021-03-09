b1296ec114776bb413fdb8695321bdd34226b72a
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 09:22:35 -0300

    pull morning

--- models/User.js
+++ models/User.js
@@ -7,15 +7,10 @@ const UserSchema = new mongoose.Schema({
   rol:{type: String, required:false, default:"user"},
   pic:{type: String, required:false, default:'../assets/img/logoUser.png'},
   password:{type: String , required: true},
-<<<<<<< HEAD
-  idShopCart:{type: mongoose.Schema.ObjectId, ref: 'shoppingCart',required: false}
-=======
   accountGoogle:{type: Boolean,default:false},
   idShopCart:{type: mongoose.Schema.ObjectId, ref: 'shoppingCart',required: false},
   requestResetPassword:{type:Number,require:false,default:0},
   tokenResetPassword:{type:String,require:false,default:''}
-
->>>>>>> 82bb23d573f3b2730e798ff15757eec7cb9a9e58
 })
 
 const User = mongoose.model('user',UserSchema)
